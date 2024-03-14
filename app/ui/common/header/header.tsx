9ce75818d9b3dd110bb968bb1b3287771d69797e
illhrek <109074837+illgreek@users.noreply.github.com>
2024-03-14 18:20:01 +0200

    adaptive header + stiky

--- app/ui/common/header/header.tsx
+++ app/ui/common/header/header.tsx
@@ -1,18 +1,16 @@
-"use client";
+'use client';
 
 import React, { useEffect, useState } from 'react';
-// @ts-ignore
 import Link from 'next/link';
 import useExchangeRate from "../../../data/API-rates/useExchangeRate";
 import Skeleton from './skeleton';
 import getUserCurrencyCode from '../../../data/API-rates/getUserCurrencyCode';
-import Image from "next/image";
 
 const Header: React.FC = () => {
-    const exchangeRate = useExchangeRate(); // отримуємо обмінний курс
+    const exchangeRate = useExchangeRate(); // получаем обменный курс
     const [userCurrencyCode, setUserCurrencyCode] = useState<string | null>(null);
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isSmallScreen, setIsSmallScreen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false); // состояние для открытия/закрытия меню
+    const [isSticky, setIsSticky] = useState(false); // состояние для "стиким" хедера
 
     useEffect(() => {
         const fetchUserCurrencyCode = async () => {
@@ -21,45 +19,96 @@ const Header: React.FC = () => {
         };
 
         fetchUserCurrencyCode();
-    }, []);
 
-    useEffect(() => {
-        const checkScreenSize = () => {
-            setIsSmallScreen(window.innerWidth < 880);
-        };
-
-        checkScreenSize();
-
-        window.addEventListener('resize', checkScreenSize);
-
+        // Добавляем слушатель событий для определения "стиким" хедера
+        window.addEventListener('scroll', handleScroll);
         return () => {
-            window.removeEventListener('resize', checkScreenSize);
+            window.removeEventListener('scroll', handleScroll);
         };
     }, []);
+
+    const handleScroll = () => {
+        if (window.scrollY > 50) {
+            setIsSticky(true);
+        } else {
+            setIsSticky(false);
+        }
+    };
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
     return (
-        <header className="bg-white">
+        <header className={`bg-white ${isSticky ? 'fixed top-0 left-0 right-0 z-50' : ''}`}>
             <div className="container mx-auto flex justify-between items-center py-4 px-8">
-                <div className="flex items-center space-x-4 gap-20">
+                <div className="flex justify-between w-full lg:w-auto items-center space-x-4 gap-20">
                     <Link className="text-xl font-bold" href="/">
-                        <Image src={"/logo.png"} width='93' height='58' alt={"logo"}/>
+                        <img src={"/logo.png"} alt={"logo"}/>
                     </Link>
-                    {/* Бургер меню */}
-                    <button
-                        className={`md:hidden text-xl text-dark_blue ${isMenuOpen ? 'transform rotate-90' : ''}`}
-                        onClick={toggleMenu}
-                        aria-label="Toggle Menu"
-                    >
-                        ☰
-                    </button>
-                    {/* Попап меню */}
-                    {isMenuOpen && isSmallScreen && (
-                        <div className="md:hidden absolute top-16 right-4 bg-white p-4 rounded shadow-lg z-10">
-                            <nav className="flex flex-col items-center gap-4">
+
+                    {/* Burger menu icon */}
+                    <div className="block lg:hidden">
+                        <button
+                            className="text-gray-700 hover:text-gray-900 focus:outline-none"
+                            onClick={toggleMenu}
+                        >
+                            {isMenuOpen ? (
+                                <svg
+                                    className="w-6 h-6"
+                                    fill="none"
+                                    stroke="#05073C"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
+                                </svg>
+                            ) : (
+                                <svg
+                                    className="w-6 h-6"
+                                    fill="none"
+                                    stroke="#05073C"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                </svg>
+                            )}
+                        </button>
+                    </div>
+
+                    {/* Navigation links for larger screens */}
+                    <nav className="hidden lg:flex items-center space-x-4 text-dark_blue gap-8">
+                        <Link className="text-dark_blue hover:text-gray-700" href="/">
+                            Home
+                        </Link>
+                        <Link className="text-dark_blue hover:text-gray-700" href="/tour">
+                            Tour
+                        </Link>
+                        <Link className="text-dark_blue hover:text-gray-700" href="/destination">
+                            Destination
+                        </Link>
+                        <Link className="text-dark_blue hover:text-gray-700" href="/contact">
+                            Contact
+                        </Link>
+                    </nav>
+                </div>
+
+                {/* Menu for smaller screens */}
+                <div className="lg:hidden">
+                    {isMenuOpen && (
+                        <div className="absolute top-16 right-0 bg-white shadow-md py-4 px-8">
+                            <nav className="flex flex-col space-y-4">
                                 <Link className="text-dark_blue hover:text-gray-700" href="/">
                                     Home
                                 </Link>
@@ -72,45 +121,30 @@ const Header: React.FC = () => {
                                 <Link className="text-dark_blue hover:text-gray-700" href="/contact">
                                     Contact
                                 </Link>
-                                {/* Курс доллара */}
-                                {!exchangeRate || !userCurrencyCode ? (
-                                    <Skeleton />
-                                ) : (
-                                    <button className="bg-orange font-bold py-2 px-4 rounded text-white pointer-events-none">
-                                        1 USD = {exchangeRate?.conversion_rates[userCurrencyCode]} {userCurrencyCode}
-                                    </button>
-                                )}
                             </nav>
+
+                            {/* Exchange rate */}
+                            {!exchangeRate || !userCurrencyCode ? (
+                                <Skeleton />
+                            ) : (
+                                <div className="bg-orange font-bold py-2 px-4 rounded text-white pointer-events-none mt-4">
+                                    1 USD = {exchangeRate?.conversion_rates[userCurrencyCode]} {userCurrencyCode}
+                                </div>
+                            )}
                         </div>
                     )}
-                    {/* Звичайне меню на планшетах і вище */}
-                    <nav className={`hidden md:flex items-center space-x-4 text-dark_blue gap-8 ${isSmallScreen ? 'hidden' : 'flex'}`}>
-                        <Link className="hover:text-gray-700" href="/">
-                            Home
-                        </Link>
-                        <Link className="hover:text-gray-700" href="/tour">
-                            Tour
-                        </Link>
-                        <Link className="hover:text-gray-700" href="/destination">
-                            Destination
-                        </Link>
-                        <Link className="hover:text-gray-700" href="/contact">
-                            Contact
-                        </Link>
-                    </nav>
                 </div>
-                {/* Курс доллара в правом верхнем углу */}
-                {!isSmallScreen && (
-                    <div>
-                        {!exchangeRate || !userCurrencyCode ? (
-                            <Skeleton />
-                        ) : (
-                            <button className="bg-orange font-bold py-2 px-4 rounded text-white pointer-events-none">
-                                1 USD = {exchangeRate?.conversion_rates[userCurrencyCode]} {userCurrencyCode}
-                            </button>
-                        )}
-                    </div>
-                )}
+
+                {/* Exchange rate for larger screens */}
+                <div className="hidden lg:block">
+                    {!exchangeRate || !userCurrencyCode ? (
+                        <Skeleton />
+                    ) : (
+                        <button className="bg-orange font-bold py-2 px-4 rounded text-white pointer-events-none">
+                            1 USD = {exchangeRate?.conversion_rates[userCurrencyCode]} {userCurrencyCode}
+                        </button>
+                    )}
+                </div>
             </div>
         </header>
     );
--- app/ui/home/TravelInfo.tsx
+++ app/ui/home/TravelInfo.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 const TravelInfo: React.FC = () => {
     return (
-        <div className="container mx-auto px-8 py-8" >
-            <div className="flex justify-center items-center px-28 py-12 bg-orange rounded-xl" >
+        <div className="container mx-auto px-4 sm:px-8 py-8" >
+            <div className="flex flex-col lg:flex-row justify-center items-center lg:items-start px-4 lg:px-28 py-12 bg-orange rounded-xl" >
                 {/* Left Section */}
-                <div className="w-1/2 pr-8">
-                    <h2 className="text-4xl font-bold text-white mb-4">We Make World Travel Easy</h2>
-                    <p className="text-lg text-white mb-8">
-                        Traveling under your own power and at your own pace, youll connect more meaningfully with your destination and have more fun!
+                <div className="w-full lg:w-1/2 pr-0 lg:pr-8">
+                    <h2 className="text-4xl lg:text-5xl font-bold text-white mb-4 lg:mb-8">We Make World Travel Easy</h2>
+                    <p className="text-lg lg:text-xl text-white mb-8 lg:mb-10">
+                        Traveling under your own power and at your own pace, you'll connect more meaningfully with your destination and have more fun!
                     </p>
                     <button className="bg-white hover:bg-light_gray text-orange font-bold py-4 px-6 rounded">
                         Explore Our Tours
@@ -17,33 +17,32 @@ const TravelInfo: React.FC = () => {
                 </div>
 
                 {/* Right Section */}
-                <div className="w-1/2 flex justify-start items-center gap-10">
-                    {/*Left Section*/}
-                    <div className="flex justify-around items-center w-1/2 flex-col gap-10">
+                <div className="w-full lg:w-1/2 flex justify-center lg:flex-row flex-col lg:justify-start items-center gap-4 lg:gap-10 mt-8 lg:mt-0">
+                    {/* Left Section */}
+                    <div className="flex justify-around items-center  w-full lg:w-1/2 flex-col gap-6 lg:gap-10">
                         {/* Block 1 */}
-                        <div className="text-center bg-white p-8 rounded-xl min-w-[240px]">
-                            <Image src="/t1.png" alt="Logo 1" width='60' height='60' className="w-16 h-16 mx-auto mb-4" />
-                            <p className="text-3xl font-bold text-orange">240</p>
+                        <div className="text-center bg-white p-6 lg:p-8 rounded-xl min-w-[240px] w-full lg:w-auto">
+                            <Image src="/t1.png" alt="Logo 1" width='60' height='60' className="w-16 h-16 mx-auto mb-2 lg:mb-4" />
+                            <p className="text-xl lg:text-2xl font-bold text-orange">240</p>
                             <p className="text-dark_blue">Total Destinations</p>
                         </div>
 
                         {/* Block 2 */}
-                        <div className="text-center bg-white p-8 rounded-xl min-w-[240px]">
-                            <Image src="/t2.png" alt="Logo 2" width='60' height='60' className="w-16 h-16 mx-auto mb-4" />
-                            <p className="text-3xl font-bold text-orange">92,842</p>
+                        <div className="text-center bg-white p-6 lg:p-8 rounded-xl min-w-[240px] w-full lg:w-auto">
+                            <Image src="/t2.png" alt="Logo 2" width='60' height='60' className="w-16 h-16 mx-auto mb-2 lg:mb-4" />
+                            <p className="text-xl lg:text-2xl font-bold text-orange">92,842</p>
                             <p className="text-dark_blue">Happy Customers</p>
                         </div>
                     </div>
 
                     {/* Block 3 */}
-                    <div className="text-center bg-white p-8 rounded-xl min-w-[240px]">
-                        <Image src="/t3.png" alt="Logo 3" width='60' height='60'  className="w-16 h-16 mx-auto mb-4" />
-                        <p className="text-3xl font-bold text-orange">3,672</p>
+                    <div className="text-center bg-white p-6 lg:p-8 rounded-xl min-w-[240px] w-full lg:w-auto">
+                        <Image src="/t3.png" alt="Logo 3" width='60' height='60' className="w-16 h-16 mx-auto mb-2 lg:mb-4" />
+                        <p className="text-xl lg:text-2xl font-bold text-orange">3,672</p>
                         <p className="text-dark_blue">Amazing Tours</p>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 };
