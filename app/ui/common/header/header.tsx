9fff95998d9f35260e86c9e11338c0620b61a352
illhrek <109074837+illgreek@users.noreply.github.com>
2024-03-17 15:14:32 +0200

    fix header img

--- app/ui/common/header/header.tsx
+++ app/ui/common/header/header.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import useExchangeRate from "../../../data/API-rates/useExchangeRate";
 import Skeleton from './skeleton';
 import getUserCurrencyCode from '../../../data/API-rates/getUserCurrencyCode';
+import Image from "next/image";
 
 const Header: React.FC = () => {
     const exchangeRate = useExchangeRate(); // получаем обменный курс
@@ -43,7 +44,7 @@ const Header: React.FC = () => {
             <div className="container mx-auto flex justify-between items-center py-4 px-8">
                 <div className="flex justify-between w-full lg:w-auto items-center space-x-4 gap-20">
                     <Link className="text-xl font-bold" href="/">
-                        <img src={"/logo.png"} alt={"logo"}/>
+                        <Image width="100" height="100" src={"/logo.png"} alt={"logo"}/>
                     </Link>
 
                     {/* Burger menu icon */}
